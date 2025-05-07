a38bbe02a88cc6fab3f0af957a425b19a8cb8ef4
Daniel <daniescu2005@gmail.com>
2025-05-07 20:50:27 +0200

    Roles, controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,49 +1,39 @@
-import { UserService, IUserService } from '@services/userService'
-import {Router} from 'express'
-import { User } from '@models/User'
-import { UserRepository, IUserRepository } from '@repositories/UserRepository'
+import { Router } from "express";
+import {
+  createUser,
+  deleteUser,
+  findUsers,
+  findUsersById,
+  updateUser,
+} from "@controllers/UserController";
+import {
+  createRole,
+  deleteRole,
+  findRoleById,
+  findRoles,
+  updateRole,
+} from "@controllers/RoleController";
 
-const router = Router()
-
-const userRepository: IUserRepository = new UserRepository()
-const userService: IUserService = new UserService(userRepository)
+const router = Router();
 
 export default () => {
-    router.get("/health", (req, res)=>{
-        res.send("Api is Healty!!!")
-    })
+  router.get("/health", (req, res) => {
+    res.send("Api is Healty!!!");
+  });
 
-    // GET USERS
-    router.get("/users", async(req, res) => {
-        const users = await userService.findUsers()
-        res.json(users)
-    })
+  // USERS ROUTES
+  router.get("/users", findUsers);
+  router.get("/users/:id", findUsersById);
+  router.post("/users", createUser);
+  router.put("/users/:id", updateUser);
+  router.delete("/users/:id", deleteUser);
 
-    // GET USER BY ID
-    router.get("/users/:id", async(req, res) => {
-        const users = await userService.findUsersById(req.params.id)
-        res.json(users)
-    })
+  // ROLES ROUTES
+  router.get("/roles", findRoles);
+  router.get("/roles/:id", findRoleById);
+  router.post("/roles", createRole);
+  router.put("/roles/:id", updateRole);
+  router.delete("/roles/:id", deleteRole);
 
-
-    // CREATE
-    router.post("/users", async(req, res) =>{
-        const newUser: User = req.body
-        const result = await userService.createUser(newUser)
-
-        res.json(result)
-    })
-
-    // UPDATE USER BY ID
-    router.put("/users/:id", async(req, res) => {
-        const user = await userService.updateUser(req.params.id, req.body)
-        res.json(user)
-    })
-
-    router.delete("/users/:id", async(req, res) => {
-        const user = await userService.deleteUser(req.params.id)
-        res.json(user)
-    })
-
-    return router;
-}
+  return router;
+};
